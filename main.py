f668804ed30e25c0cbecd9d80045fada6bc9e329
dianaalitzel <85376055+dianaalitzel@users.noreply.github.com>
2021-07-27 20:05:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,147 +9,153 @@ from colorama import Fore, init, Style, Back
 init()
 
 
-ciclo = True
-while ciclo == True:
+while True:
     try:
         print(Fore.GREEN + "CONSUMO DE APIS")
         print(Fore.YELLOW + "APIS: \n 1.- Usar ipstack \n 2.- Usar ipapi \n 0.- Salir") 
 
         print(Fore.WHITE + " ")
         menu = int(input("Seleccionar API: "))
+        ciclo = True
 
         if menu == 1:
-            try:
-                print(" ")
-                print(Fore.GREEN + "SELECCION DE DIRECCIONES")
-                print(Fore.YELLOW + "Tipo de dirección: \n 1.- IPv4 \n 2.- IPv6") 
-            
-                print(Fore.WHITE + " ")
-                op = int(input("Seleccionar tipo de dirección: "))
+            while ciclo:
+                try:
+                    print(" ")
+                    print(Fore.GREEN + "SELECCION DE DIRECCIONES")
+                    print(Fore.YELLOW + "Tipo de dirección: \n 1.- IPv4 \n 2.- IPv6") 
 
-                if op == 1:
-                    response = nin.ipstack_info()
-                    items=response.items()
-                    mylist=list(items)
+                    print(Fore.WHITE + " ")
+                    op = int(input("Seleccionar tipo de dirección: "))
 
-                    print(" ")
-                    bar2 = ChargingBar('Obteniendo informacion:', max=100)
-                    for num in range(100):
-                        time.sleep(random.uniform(0, 0.001))
-                        bar2.next()
-                    bar2.finish()
-                    print(" ")
-                    
-                    print(Fore.GREEN + "Tabla de resultados desde IPSTACK")
-                    extra=(mylist.pop(-1))
-                    localizacion=(extra)
-                    location=(localizacion[1].items())
-                    print(Style.RESET_ALL + Fore.CYAN + tabulate(mylist, tablefmt='fancy_grid'))
-                    print(" ")
-                    print(Fore.GREEN + "Localizacion")
-                    print(Style.RESET_ALL + Fore.CYAN + tabulate(location, tablefmt='fancy_grid'))
-                    print(" ")
+                    if op == 1:
+                        ciclo = False
+                        response = nin.ipstack_info()
+                        items=response.items()
+                        mylist=list(items)
 
-                    
-                elif op == 2:
-                    response = nin.ipstack_info()
-                    items=response.items()
-                    mylist=list(items)
+                        print(" ")
+                        bar2 = ChargingBar('Obteniendo informacion:', max=100)
+                        for num in range(100):
+                            time.sleep(random.uniform(0, 0.001))
+                            bar2.next()
+                        bar2.finish()
+                        print(" ")
 
-                    print(" ")
-                    bar2 = ChargingBar('Obteniendo informacion:', max=100)
-                    for num in range(100):
-                        time.sleep(random.uniform(0, 0.001))
-                        bar2.next()
-                    bar2.finish()
-                    print(" ")
-                    
-                    print(Fore.GREEN + "Tabla de resultados desde IPSTACK")
-                    extra=(mylist.pop(-1))
-                    localizacion=(extra)
-                    location=(localizacion[1].items())
-                    print(Style.RESET_ALL + Fore.CYAN + tabulate(mylist, tablefmt='fancy_grid'))
-                    print(" ")
-                    print(Fore.GREEN + "Localizacion")
-                    print(Style.RESET_ALL + Fore.CYAN + tabulate(location, tablefmt='fancy_grid'))
-                    print(" ")
+                        print(Fore.GREEN + "Tabla de resultados desde IPSTACK")
+                        extra=(mylist.pop(-1))
+                        localizacion=(extra)
+                        location=(localizacion[1].items())
+                        print(Style.RESET_ALL + Fore.CYAN + tabulate(mylist, tablefmt='fancy_grid'))
+                        print(" ")
+                        print(Fore.GREEN + "Localizacion")
+                        print(Style.RESET_ALL + Fore.CYAN + tabulate(location, tablefmt='fancy_grid'))
+                        print(" ")
 
-                else:
+
+                    elif op == 2:
+                        ciclo = False
+                        response = nin.ipstack_info()
+                        items=response.items()
+                        mylist=list(items)
+
+                        print(" ")
+                        bar2 = ChargingBar('Obteniendo informacion:', max=100)
+                        for num in range(100):
+                            time.sleep(random.uniform(0, 0.001))
+                            bar2.next()
+                        bar2.finish()
+                        print(" ")
+
+                        print(Fore.GREEN + "Tabla de resultados desde IPSTACK")
+                        extra=(mylist.pop(-1))
+                        localizacion=(extra)
+                        location=(localizacion[1].items())
+                        print(Style.RESET_ALL + Fore.CYAN + tabulate(mylist, tablefmt='fancy_grid'))
+                        print(" ")
+                        print(Fore.GREEN + "Localizacion")
+                        print(Style.RESET_ALL + Fore.CYAN + tabulate(location, tablefmt='fancy_grid'))
+                        print(" ")
+
+                    else:
+                        print(" ")
+                        print(Style.RESET_ALL + Fore.RED + "Lo sentimos no contamos con esa opcion :c")
+                        print(" ")
+
+                except ValueError:
+                    print (" ")
+                    print(Style.RESET_ALL + Fore.RED + "!Numero invalido!")
                     print(" ")
-                    print(Style.RESET_ALL + Fore.RED + "Lo sentimos no contamos con esa opcion :c")
-                    print(" ")
-                    
-            except ValueError:
-                print (" ")
-                print(Style.RESET_ALL + Fore.RED + "!Numero invalido!")
-                print(" ")
 
         #Ricardo 23/07/2021        
         elif menu == 2:
-            try:
-                print(" ")
-                print(Fore.GREEN + "SELECCION DE DIRECCIONES")
-                print(Fore.YELLOW + "Tipo de dirección: \n 1.- IPv4 \n 2.- IPv6")
-                
-                print(Fore.WHITE + " ")
-                op = int(input("Seleccionar tipo de dirección: "))
-                
+            while ciclo:
+                try:
+                    print(" ")
+                    print(Fore.GREEN + "SELECCION DE DIRECCIONES")
+                    print(Fore.YELLOW + "Tipo de dirección: \n 1.- IPv4 \n 2.- IPv6")
 
-                if op == 1:
-                    response = dani.ipapi_info()
-                    items=response.items()
-                    mylist=list(items)
+                    print(Fore.WHITE + " ")
+                    op = int(input("Seleccionar tipo de dirección: "))
 
-                    print(" ")
-                    bar2 = ChargingBar('Obteniendo informacion:', max=100)
-                    for num in range(100):
-                        time.sleep(random.uniform(0, 0.001))
-                        bar2.next()
-                    bar2.finish()
-                    print(" ")
-                    
-                    print(Fore.GREEN + "Tabla de resultados desde IPAPI")
-                    extra=(mylist.pop(-1))
-                    localizacion=(extra)
-                    location=(localizacion[1].items())
-                    print(Style.RESET_ALL + Fore.CYAN + tabulate(mylist, tablefmt='fancy_grid'))
-                    print(" ")
-                    print(Fore.GREEN + "Localizacion")
-                    print(Style.RESET_ALL + Fore.CYAN + tabulate(location, tablefmt='fancy_grid'))
-                    print(" ")
-                    
-                elif op == 2:
-                    response = dani.ipapi_info()
-                    items=response.items()
-                    mylist=list(items)
 
-                    print(" ")
-                    bar2 = ChargingBar('Obteniendo informacion:', max=100)
-                    for num in range(100):
-                        time.sleep(random.uniform(0, 0.001))
-                        bar2.next()
-                    bar2.finish()
-                    print(" ")
-                    
-                    print(Fore.GREEN + "Tabla de resultados desde IPAPI")
-                    extra=(mylist.pop(-1))
-                    localizacion=(extra)
-                    location=(localizacion[1].items())
-                    print(Style.RESET_ALL + Fore.CYAN + tabulate(mylist, tablefmt='fancy_grid'))
-                    print(" ")
-                    print(Fore.GREEN + "Localizacion")
-                    print(Style.RESET_ALL + Fore.CYAN + tabulate(location, tablefmt='fancy_grid'))
-                    print(" ")
+                    if op == 1:
+                        ciclo = False
+                        response = dani.ipapi_info()
+                        items=response.items()
+                        mylist=list(items)
 
-                else:
+                        print(" ")
+                        bar2 = ChargingBar('Obteniendo informacion:', max=100)
+                        for num in range(100):
+                            time.sleep(random.uniform(0, 0.001))
+                            bar2.next()
+                        bar2.finish()
+                        print(" ")
+
+                        print(Fore.GREEN + "Tabla de resultados desde IPAPI")
+                        extra=(mylist.pop(-1))
+                        localizacion=(extra)
+                        location=(localizacion[1].items())
+                        print(Style.RESET_ALL + Fore.CYAN + tabulate(mylist, tablefmt='fancy_grid'))
+                        print(" ")
+                        print(Fore.GREEN + "Localizacion")
+                        print(Style.RESET_ALL + Fore.CYAN + tabulate(location, tablefmt='fancy_grid'))
+                        print(" ")
+
+                    elif op == 2:
+                        ciclo = False
+                        response = dani.ipapi_info()
+                        items=response.items()
+                        mylist=list(items)
+
+                        print(" ")
+                        bar2 = ChargingBar('Obteniendo informacion:', max=100)
+                        for num in range(100):
+                            time.sleep(random.uniform(0, 0.001))
+                            bar2.next()
+                        bar2.finish()
+                        print(" ")
+
+                        print(Fore.GREEN + "Tabla de resultados desde IPAPI")
+                        extra=(mylist.pop(-1))
+                        localizacion=(extra)
+                        location=(localizacion[1].items())
+                        print(Style.RESET_ALL + Fore.CYAN + tabulate(mylist, tablefmt='fancy_grid'))
+                        print(" ")
+                        print(Fore.GREEN + "Localizacion")
+                        print(Style.RESET_ALL + Fore.CYAN + tabulate(location, tablefmt='fancy_grid'))
+                        print(" ")
+
+                    else:
+                        print(" ")
+                        print(Style.RESET_ALL + Fore.RED + "Lo sentimos no contamos con esa opcion :c")
+                        print(" ")
+
+                except ValueError:
+                    print (" ")
+                    print(Style.RESET_ALL + Fore.RED + "!Numero invalido!")
                     print(" ")
-                    print(Style.RESET_ALL + Fore.RED + "Lo sentimos no contamos con esa opcion :c")
-                    print(" ")
-                    
-            except ValueError:
-                print (" ")
-                print(Style.RESET_ALL + Fore.RED + "!Numero invalido!")
-                print(" ")
                 
         elif menu == 0:
             print(" ")
@@ -167,5 +173,4 @@ while ciclo == True:
         print(" ")
         print(Style.RESET_ALL + Fore.RED + "Caracter invalido")
         print("")
-        ciclo = True
 
